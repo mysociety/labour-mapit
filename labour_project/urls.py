365b571d84249b4eac316b904afa6339f280d605
Dave Arter <davea@mysociety.org>
2021-12-03 20:13:31 +0000

    Basic project to import & display AddressBase Core

---
+++ labour_project/urls.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.urls import include, path, re_path
+from django.contrib import admin
+
+handler500 = 'mapit.shortcuts.json_500'
+
+urlpatterns = [
+    re_path(r'^', include('mapit.urls')),
+    re_path(r'^', include('mapit_labour.urls')),
+    re_path(r'^admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns.append(path('__debug__/', include(debug_toolbar.urls)))
