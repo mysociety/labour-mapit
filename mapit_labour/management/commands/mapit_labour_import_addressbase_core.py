2159e521ff185f4c784fe8b061719a17dbb80801
Dave Arter <davea@mysociety.org>
2021-12-08 23:05:07 +0000

    Improve memory efficiency and speed when importing AddressBase Core

--- mapit_labour/management/commands/mapit_labour_import_addressbase_core.py
+++ mapit_labour/management/commands/mapit_labour_import_addressbase_core.py
@@ -1,14 +1,33 @@
-from itertools import groupby
+from itertools import groupby, islice
 from typing import Dict
+from collections import deque
 
+from django.conf import settings
 from django.core.management.base import LabelCommand
 from django.contrib.gis.geos import Point
-from django.db import transaction, IntegrityError
+from django.db import transaction, connection
 
 from csv import DictReader
 
 
 from mapit_labour.models import UPRN
+
+if settings.DEBUG:
+    # Disable the Django SQL query log, which eats memory.
+    # It's normally cleared after a request but we're not
+    # running in a request-response cycle here so it just keeps
+    # growing and will cause problems when importing this much data.
+    connection.queries_log = deque(maxlen=0)
+
+
+def batched(iterable, size):
+    """
+    Split an iterable into smaller iterables no bigger than size
+    """
+    return (
+        (g for _, g in item)
+        for _, item in groupby(enumerate(iterable), key=lambda x: x[0] // size)
+    )
 
 
 class Command(LabelCommand):
@@ -22,6 +41,7 @@ class Command(LabelCommand):
     }
     batch_size = 1000
     purge = False
+    limit = 0
 
     def add_arguments(self, parser):
         super(Command, self).add_arguments(parser)
@@ -29,12 +49,28 @@ class Command(LabelCommand):
             "--purge",
             action="store_true",
             dest="purge",
-            default=False,
+            default=self.purge,
             help="Purge all existing UPRNs and import afresh",
+        )
+        parser.add_argument(
+            "--batch-size",
+            dest="batch_size",
+            type=int,
+            default=self.batch_size,
+            help=f"Batch size for bulk INSERT/UPDATE operations. Default {self.batch_size}",
+        )
+        parser.add_argument(
+            "--limit",
+            dest="limit",
+            type=int,
+            default=self.limit,
+            help="Stop after importing this many rows from the CSV. 0 (default) for no limit.",
         )
 
     def handle_label(self, label: str, **options):
         self.purge = options["purge"]
+        self.batch_size = options["batch_size"]
+        self.limit = options["limit"]
 
         with open(label, encoding="utf-8-sig") as f:
             self.handle_rows(DictReader(f))
@@ -43,25 +79,20 @@ class Command(LabelCommand):
         if self.purge:
             UPRN.objects.all().delete()
 
-            for _, uprns in groupby(
-                (self.create_uprn(row) for row in csv),
-                lambda _: self.count["total"] // self.batch_size,
-            ):
+            for rows in batched(islice(csv, self.limit or None), self.batch_size):
                 with transaction.atomic():
-                    UPRN.objects.bulk_create(uprns)
+                    UPRN.objects.bulk_create(self.create_uprn(row) for row in rows)
                 self.print_stats()
         self.print_stats()
 
     def create_uprn(self, row: Dict[str, str]):
-        row = {k.lower(): v for k, v in row.items()}
-
         self.count["total"] += 1
         self.count["created"] += 1
         return UPRN(
-            uprn=row["uprn"],
-            postcode=row["postcode"].replace(" ", ""),
-            location=Point(float(row["easting"]), float(row["northing"]), srid=27700),
-            single_line_address=row["single_line_address"],
+            uprn=row["UPRN"],
+            postcode=row["POSTCODE"].replace(" ", ""),
+            location=Point(float(row["EASTING"]), float(row["NORTHING"]), srid=27700),
+            single_line_address=row["SINGLE_LINE_ADDRESS"],
             addressbase=row,
         )
 
