4a4eed190673591c40a7dafa5305be4e764ba7fc
Dave Arter <davea@mysociety.org>
2022-04-01 21:46:00 +0100

    Add Django Q for task queue processing

--- mapit_labour/importers.py
+++ mapit_labour/importers.py
@@ -1,7 +1,4 @@
-from pprint import pprint
 from csv import DictReader
-from re import T
-from sqlite3 import IntegrityError
 
 from django.db import transaction
 
@@ -12,23 +9,51 @@ class BranchCSVImporter:
     commit = False
     purge = False
     path = None
+    generation = None
 
-    def __init__(self, path, purge=False, commit=False):
+    created = 0
+    updated = 0
+    warnings = None
+    error = None
+
+    def __init__(self, path, purge=False, commit=False, generation=None):
         self.path = path
         self.purge = purge
         self.commit = commit
 
+        if not generation:
+            self.generation = Generation.objects.current()
+        else:
+            try:
+                self.generation = Generation.objects.get(generation)
+            except Generation.DoesNotExist:
+                raise ValueError("Invalid generation number specified")
+
+        self.warnings = []
+
     @classmethod
-    def import_from_csv(cls, path, purge, commit):
-        return BranchCSVImporter(path, purge=purge, commit=commit).do_import()
+    def import_from_csv(cls, path, purge, commit, generation):
+        importer = BranchCSVImporter(
+            path, purge=purge, commit=commit, generation=generation
+        )
+        importer.do_import()
+        return {
+            "created": importer.created,
+            "updated": importer.updated,
+            "warnings": importer.warnings,
+            "error": importer.error,
+        }
 
     @transaction.atomic
     def do_import(self):
         if self.purge:
             Area.objects.filter(type__code="LBR").delete()
 
-        with open(self.path, encoding="utf-8-sig") as f:
-            self.handle_rows(DictReader(f))
+        try:
+            with open(self.path, encoding="utf-8-sig") as f:
+                self.handle_rows(DictReader(f))
+        except Exception as e:
+            self.error = str(e)
 
         if not self.commit:
             transaction.set_rollback(True)
@@ -51,8 +76,6 @@ class BranchCSVImporter:
                 )
 
     def handle_rows(self, csv: DictReader):
-        # XXX do the right thing with generations
-        generation = Generation.objects.current()
         gss_codetype = CodeType.objects.get(code="gss")
         lbr_codetype = CodeType.objects.get(code="lbr")
 
@@ -70,16 +93,18 @@ class BranchCSVImporter:
                     )
                 )
             except Area.DoesNotExist:
-                print(
+                self.warnings.append(
                     f"Invalid row on line {i}: Subarea with GSS code '{row['gss_code']}' doesn't exist."
                 )
 
         for branch in branches.values():
+            # TODO handle updating existing branches
             a = Area.objects.create(
                 name=branch["area_name"],
                 type=Type.objects.get(code=branch["area_type"]),
-                generation_high=generation,
-                generation_low=generation,
+                # XXX do the right thing with generations
+                generation_high=self.generation,
+                generation_low=self.generation,
             )
             a.codes.update_or_create(
                 type=gss_codetype, defaults={"code": branch["area_gss"]}
@@ -92,8 +117,9 @@ class BranchCSVImporter:
                     codes__type=gss_codetype, codes__code=branch["parent_gss_code"]
                 )
                 a.save()
+                self.created += 1
             except Area.DoesNotExist:
-                print(
+                self.warnings.append(
                     f"Parent area with GSS code '{branch['parent_gss_code']}' does not exist."
                 )
                 a.delete()
@@ -113,7 +139,9 @@ class BranchCSVImporter:
                     # Sometimes the intersection results in a MultiPolygon, which
                     # we'll need to store as separate Polygon geometries
                     if p.geom_type == "MultiPolygon":
-                        print(f"Multis for area {a.id} (branch {branch['area_id']})")
+                        self.warnings.append(
+                            f"Multis for area {a.id} (branch {branch['area_id']})"
+                        )
                         for poly in p:
                             a.polygons.create(polygon=poly)
                             has_geometry = True
@@ -123,8 +151,10 @@ class BranchCSVImporter:
                         has_geometry = True
                         area_area += p.area
             if not has_geometry:
-                print(f"Area {a.id} (branch {branch['area_id']}) has no geometry!")
+                self.warnings.append(
+                    f"Area {a.id} (branch {branch['area_id']}) has no geometry!"
+                )
             elif area_area < 50000:
-                print(
+                self.warnings.append(
                     f"Area {a.id} (branch {branch['area_id']}) has a small geographic area ({int(area_area)} ㎡)!"
                 )
