121a5271971968548774720a0704e693f029f1e8
Dave Arter <davea@mysociety.org>
2022-04-01 20:54:08 +0100

    Switch to PyMemcache; allow memcache to be used in DEBUG mode

--- labour_project/settings.py
+++ labour_project/settings.py
@@ -45,7 +45,7 @@ DEBUG = config.get('DEBUG', True)
 
 # (Note that even if DEBUG is true, output_json still sets a
 # Cache-Control header with max-age of 28 days.)
-if DEBUG:
+if DEBUG and not config.get('DEBUG_USE_MEMCACHE', False):
     CACHES = {
         'default': {
             'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
@@ -54,10 +54,10 @@ if DEBUG:
     CACHE_MIDDLEWARE_SECONDS = 0
 else: # pragma: no cover
     try:
-        import memcache
+        import pymemcache
         CACHES = {
             'default': {
-                'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+                'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
                 'LOCATION': '127.0.0.1:11211',
                 'TIMEOUT': 86400,
             }
