8f07a4b3a499ef1a06f895423aa5769448b17445
Dave Arter <davea@mysociety.org>
2022-04-05 14:18:51 +0100

    Improve import form & auto reload task pending page

--- mapit_labour/forms.py
+++ mapit_labour/forms.py
@@ -6,13 +6,35 @@ from django.conf import settings
 
 from django_q.tasks import async_task
 
+from mapit.models import Generation
+
 from mapit_labour.importers import BranchCSVImporter
+
+
+def get_generation_choices():
+    g = Generation.objects.current()
+    return [(g.id, f"{g.id}: {g.description}")]
 
 
 class ImportCSVForm(forms.Form):
     file = forms.FileField(required=True, allow_empty_file=False)
-    commit = forms.BooleanField(initial=False, required=False)
-    purge = forms.BooleanField(initial=True, required=False)
+    commit = forms.BooleanField(
+        initial=False,
+        required=False,
+        help_text="Leave unticked to do a dry-run and not change the database.",
+    )
+    purge = forms.BooleanField(
+        initial=True, required=False, label="Replace existing areas", disabled=True
+    )
+    generation_choice = forms.ChoiceField(
+        choices=get_generation_choices,
+        disabled=True,
+        required=False,
+        label="Generation",
+    )
+    generation = forms.Field(
+        widget=forms.HiddenInput, initial=lambda: get_generation_choices()[0][0]
+    )
 
     def add_import_task(self):
         data = self.cleaned_data
