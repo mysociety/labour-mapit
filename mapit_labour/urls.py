864480a42d2069f0137904bcec1c0c4d44643783
Dave Arter <davea@mysociety.org>
2023-11-28 09:21:54 +0000

    Use django.urls.re_path instead of django.conf.urls.url
    Deprecated since Django 3.1 and removed in Django 4

--- mapit_labour/urls.py
+++ mapit_labour/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import re_path, path
 
 from mapit_labour.views import (
     uprn,
@@ -13,20 +13,20 @@ from mapit_labour.views import (
 format_end = r"(?:\.(?P<format>html|json))?"
 
 urlpatterns = [
-    url(r"^uprn/(?P<uprn>[0-9]+)%s$" % format_end, uprn, name="mapit_labour-uprn"),
-    url(r"^addressbase$", addressbase, name="mapit_labour-addressbase"),
-    url(r"^import/csv/?$", import_csv, name="mapit_labour-import_csv"),
-    url(
+    re_path(r"^uprn/(?P<uprn>[0-9]+)%s$" % format_end, uprn, name="mapit_labour-uprn"),
+    path("addressbase", addressbase, name="mapit_labour-addressbase"),
+    path("import/csv", import_csv, name="mapit_labour-import_csv"),
+    re_path(
         r"^import/csv/(?P<task_id>[0-9a-f]+)$",
         import_csv_status,
         name="mapit_labour-import_csv_status",
     ),
-    url(r"^health$", health_check),
+    path("health", health_check),
     # Override the existing mapit.views.areas.area view with our own that
     # supports lookup of branches/regions by GSS code. Necessary because
     # the pseudo-GSS codes assigned to these areas don't match the
     # pattern being looked for in mapit_gb.countries.area_code_lookup.
     # URL pattern deliberately limits to LR_/BR_ prefix so most hits should
     # go straight to original view function.
-    url(r"^area/(?P<area_id>[BL]R_[0-9A-Z_]+)%s$" % format_end, area),
+    re_path(r"^area/(?P<area_id>[BL]R_[0-9A-Z_]+)%s$" % format_end, area),
 ]
